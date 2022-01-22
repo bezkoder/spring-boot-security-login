7c78d62f368c22e52d01fdbc9a9e69b7a66ced5f
tienbku <tien.ngtruong@gmail.com>
2022-01-22 19:12:09 +0700

    first commit

---
+++ src/test/java/com/bezkoder/spring/security/login/SpringBootSecurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.security.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
